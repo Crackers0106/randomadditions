8fa81b7081a1714f22071e2b62c2cc757accb420
Crackers0106 <32655060+Crackers0106@users.noreply.github.com>
2021-09-27 13:12:57 -0400

    Change: Finish removing Peels

--- src/main/java/net/crackers0106/randomadditions/item/BananaItem.java
+++ src/main/java/net/crackers0106/randomadditions/item/BananaItem.java
@@ -16,13 +16,13 @@ public class BananaItem extends Item {
 	public BananaItem(Settings settings) {
 		super(settings);
 	}
-
-	public ItemStack finishUsing(ItemStack stack, World world, LivingEntity user) { // fires when finished using
-		if (stack.isItemEqual(RAItems.PEELED_BANANA.getDefaultStack()) && !((PlayerEntity) user).getAbilities().creativeMode) { // this is probs unsafe if non players eat food
-			user.dropItem(RAItems.BANANA_PEEL); // theres probs a way to give it directly to player inventory or modify the pickup function for this item to make the player "slip" when they collide
-		}
-		return super.finishUsing(stack, world, user);
-	}
+//
+//	public ItemStack finishUsing(ItemStack stack, World world, LivingEntity user) { // fires when finished using
+//		if (stack.isItemEqual(RAItems.PEELED_BANANA.getDefaultStack()) && !((PlayerEntity) user).getAbilities().creativeMode) { // this is probs unsafe if non players eat food
+//			user.dropItem(RAItems.BANANA_PEEL); // theres probs a way to give it directly to player inventory or modify the pickup function for this item to make the player "slip" when they collide
+//		}
+//		return super.finishUsing(stack, world, user);
+//	}
 
 	@SuppressWarnings({ "unchecked", "rawtypes" })
 	public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) { // fires when player right clicks
